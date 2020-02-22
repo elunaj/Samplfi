1d4318076d94715bc858a3915e5a7d1afd093861
elunaj <elias.luna@mail.citytech.cuny.edu>
2020-02-21 20:36:00 -0500

    deploy frontend

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -23,7 +23,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignin = () => {
-		fetch('http://localhost:5000/signin', {
+		fetch('https://peaceful-sierra-85182.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
